b9278355252c491d566d0f719a8ca53af204bfca
Mate <gav.sqrt@gmail.com>
2018-09-20 11:00:21 +0300

    add gtm

--- header.php
+++ header.php
@@ -1,6 +1,13 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?> >
 <head>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-5CC8JNN');</script>
+    <!-- End Google Tag Manager -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -22,9 +29,13 @@
 </head>
 
 <body>
-
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5CC8JNN"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 <?php get_template_part('includes/preloader');?>
 
 <?php get_template_part('includes/top-menu'); ?>
 <?php get_template_part('includes/form_callback');?>
+
 
