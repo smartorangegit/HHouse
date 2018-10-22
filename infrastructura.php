77ece976d9dbbe4210e54e4146121624978d9b27
Sergey Khistovoy <sergeykhistovoy@gmail.com>
2018-10-22 10:03:18 +0300

    hid a block with children's clinics on the infrastructure page

--- infrastructura.php
+++ infrastructura.php
@@ -56,12 +56,12 @@ $img = (get_the_post_thumbnail_url($post->ID, 'full'));
                         <svg class="smile_line open" height="1.011161mm" viewBox="0 0 23.316424 1.011161" width="23.316425mm" xmlns="http://www.w3.org/2000/svg"><g transform="translate(-10.843172 -11.709181)"><path d="m10.843172 12.209181h17.681462.310205" fill="#ee1c25" stroke="#ee1c25"></path><g fill="none"><path d="m30.238531 12.220342h1.322917" stroke="#000"></path><path d="m32.836681 12.218563h1.322917" stroke="#ee1c25"></path></g></g></svg>
                     </div>
                 </div>
-                <div class="infra__item infra__item__4 wow fadeIn">
+<?/*                 <div class="infra__item infra__item__4 wow fadeIn">
                     <div class="section__name wow fadeInLeft" data-wow-duration="1.2s">
                         <h2><?=$links[3]?></h2>
                         <svg class="smile_line open" height="1.011161mm" viewBox="0 0 23.316424 1.011161" width="23.316425mm" xmlns="http://www.w3.org/2000/svg"><g transform="translate(-10.843172 -11.709181)"><path d="m10.843172 12.209181h17.681462.310205" fill="#ee1c25" stroke="#ee1c25"></path><g fill="none"><path d="m30.238531 12.220342h1.322917" stroke="#000"></path><path d="m32.836681 12.218563h1.322917" stroke="#ee1c25"></path></g></g></svg>
                     </div>
-                </div>
+                </div> */ ?>
                 <div class="infra__item infra__item__5 wow fadeIn">
                     <div class="section__name wow fadeInLeft" data-wow-duration="1.2s">
                         <h2><?=$links[4]?></h2>
